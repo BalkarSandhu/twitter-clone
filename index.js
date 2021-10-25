65a28f70dcad67994e1802b8e1c81f6bace2e3ed
Balkar <balkarsandhu321@gmail.com>
2021-10-25 11:21:19 +0530

    before connecting to db

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express= require('express');
 const router=require('./src/routes/index');
 const ejs=require('ejs');
+var expressLayouts = require('express-ejs-layouts');
 
 
 const app=express();
-app.use('/',router);
+app.use(express.static(__dirname+'/src/assets'));
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.use(expressLayouts);
+app.set('layout',__dirname+'/src/views/layouts/layout');
 app.set('view engine','ejs');
 app.set('views','./src/views');
+app.use('/',router);
+
+
+
 
 
 app.listen(3000,()=>{
